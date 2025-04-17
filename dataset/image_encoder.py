ea8aa5831a638c42f6c447b4c81b5435b79c7d2d
jiangyichen19 <1965889194@qq.com>
2025-04-17 14:19:27 +0800

    modify dataset

---
+++ dataset/image_encoder.py
@@ -0,0 +1,4 @@
+import torch
+import torch.nn as nn
+import transformers
+from transformers import BertModel, BertTokenizer, BertConfig, BertPreTrainedModel, BertForSequenceClassification, BertForMaskedLM, BertForNextSentencePredictio
